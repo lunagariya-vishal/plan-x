73fed2d42eb06ab29d496ac36f31a99867c448ae
dilip solanki <dilip.onesourcewebs@gmail.com>
2019-02-21 18:58:25 +0530

    New updates

--- public/main.js
+++ public/main.js
@@ -1862,7 +1862,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    apiHost: 'http://localhost:8080/'
+    apiHost: '/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
